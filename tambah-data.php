e985a5e7fbeef04dcb4c6658937a13157e4ffd13
Saniarinjani <saniarinjani087@gmail.com>
2024-11-24 02:08:41 -0800

    first commit

---
+++ tambah-data.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+</head>
+<body>
+    <form action="insert.php" method="get">
+        <label for="nama_barang">Nama Barang</label>
+        <input type="text" name="nama_barang"><br/>
+        <label for="harga_barang">Harga Barang</label>
+        <input type="number" name="harga_barang"><br/>
+        <button>Simpan </button>
+    </form>
+      
+</body>
+</html>
---
+++ update_data.php
@@ -0,0 +1,8 @@
+<?php
+include "koneksi.php";
+$nama_barang = $_POST['nama_barang'];
+$harga_barang = $_POST['harga_barang'];
+$query = "UPDATE barang set nama_barang='$nama_barang', harga=
+'$harga_barang' where id='" . $_GET['id'] . "'";
+$mysqli->query($query);
+header('location:index.php');
